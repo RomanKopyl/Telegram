615d91128e892bc7236fda2a69225159b91d45d1
Roman <>
2024-06-04 17:37:23 +0300

    Connect supabase user to streamchat user

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { router } from 'expo-router';
 import { Channel as ChannelType } from 'stream-chat';
 import { ChannelList } from 'stream-chat-expo';
+import { useAuth } from '../../../providers/AuthProvider';
 
 export default function MainTabScreen() {
+	const { user } = useAuth();
+
 	return (
 		<ChannelList
+			filters={{ members: { $in: [user.id] } }}
 			onSelect={(channel: ChannelType) => router.push(`/channel/${channel.cid}`)}
 		/>
 	);
