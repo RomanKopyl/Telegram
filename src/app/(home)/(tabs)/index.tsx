16d524e5a7822132f1c7b718041aa90e1236ad04
Roman <>
2024-06-11 11:23:30 +0300

    Change Link to onPress

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -11,14 +11,15 @@ export default function MainTabScreen() {
 		<>
 			<Stack.Screen options={{
 				headerRight: () =>
-					<Link href={'/(home)/users'} asChild>
-						<FontAwesome5
-							name="users"
-							size={22}
-							color="gray"
-							style={{ marginHorizontal: 15 }}
-						/>
-					</Link>
+					// <Link href={'/(home)/users'} asChild>
+					<FontAwesome5
+						onPress={() => router.push('/(home)/users')}
+						name="users"
+						size={22}
+						color="gray"
+						style={{ marginHorizontal: 15 }}
+					/>
+				// </Link>
 			}}
 			/>
 			<ChannelList
